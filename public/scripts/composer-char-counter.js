23b2e2865a5b648839650804b9add4f809e1778e
EDonnellan <elaine.donny@gmail.com>
2023-03-01 04:57:54 +0000

    Have reverse count functionality just adding to counter now.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  console.log("IS THE DOC READY?!");
+  console.log("Page is Ready!");
+
+  $("textarea").on("input", function(event) {
+    const count = $(this).val().length;
+    console.log("And what about this", $(this).val().length);
+
+    const reverseCount = 140 - count;
+    console.log("reverseCount: ", reverseCount);
+  
+    console.log("counter: ", $(".counter").output);
+})
 });
