ff18a4a294da645af916a6c15f21d80e3f35c248
EDonnellan <elaine.donny@gmail.com>
2023-03-01 06:39:50 +0000

    Implemented the counter with reverse countdown & red when negative.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
   console.log("Page is Ready!");
 
+  //Event listener && logic for tweet character counter
   $("textarea").on("input", function(event) {
     const count = $(this).val().length;
-    console.log("And what about this", $(this).val().length);
 
     const reverseCount = 140 - count;
-    console.log("reverseCount: ", reverseCount);
-  
-    console.log("counter: ", $(".counter").output);
+
+    //finding the counter withing the DOM
+    const textAreaParents = $(this).parents();
+    const directParent = textAreaParents[0];
+    const parentChildren = directParent.children;
+    const btnAndCounterDiv = parentChildren[2];
+    const btnAndCounterDivChildren = btnAndCounterDiv.children;
+    let counterChild = btnAndCounterDivChildren[1]
+
+    //reversing the count
+    $(counterChild).val(reverseCount);
+
+    //logic for colour if positive or negative number
+    if ($(counterChild).val() < 0) {
+      $(counterChild).css("color", "red")
+    } else {
+      $(counterChild).css("color", "#535149")
+    }
 })
 });
