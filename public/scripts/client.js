585d773d0fd83b1378f56cd22b3353f7d8a21acd
EDonnellan <elaine.donny@gmail.com>
2023-03-03 05:11:47 +0000

    Completed error messages & their sliding up & down.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,12 +8,14 @@
 $(document).ready(function () {
   console.log("client.js document is ready");
 
+  //escaping cross site scripting
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  //Get request for tweets
   const loadTweets = function () {
     console.log("GET FORM?!?!");
 
@@ -27,77 +29,69 @@ $(document).ready(function () {
     });
   };
 
-  loadTweets();
+  loadTweets(); //loading existing tweets on page load
 
-  // console.log($("form"), "the form")
+  //Tweet submission
   $("form").submit(function (event) {
-    // alert("Handler for .submit() called.");
-    event.preventDefault();
+    event.preventDefault();//stop page refresh
 
-    const formData = $(this).serialize();
-
-    console.log("formData", formData);
+    const formData = $(this).serialize();//ensures we're getting just the form value
 
     let tweetText = $("#tweet-text").val();
     const $error = $(".error");
-    console.log("error", $error);
+    const $errorMsg = $error.find("span");  
     
-
+    //check if tweet is empty
     if (!tweetText) {
-      $error.find("span").val("Tweet cannot be empty!");
-      console.log("$error.find('span')", $error.find("span"), $error.find("span").innerHTML);
+      $error.slideUp(1000, function () {//Make sure error msg if gone then deal with current error
+        $errorMsg.text("Tweet cannot be empty!");
+        $error.slideDown(1000);
+      });
 
-      $error.slideDown(1000);
       return;
+      //check if tweet is too long (> 140 chars)
+    } else if (tweetText.length > 140) {
+      $error.slideUp(1000, function () {//Make sure error msg if gone then deal with current error
+        $errorMsg.text("Tweet cannot be longet than 140 characters!");
+        $error.slideDown(1000);
+      });
+
+      return;
+
+    } else { //else slide up as there is no error
+      $error.slideUp(1000);
     }
-
-    if (tweetText.length > 140) {
-      // alert("NAH!");
-      // longTweet.val("<i class='fa-solid fa-triangle-exclamation'>  Tweet cannot be empty!  <i class='fa-solid fa-triangle-exclamation'>");
-      // console.log("longTweet: ", longTweet);
-
-      longTweet.slideDown(1000, function () {
-        $(".error").innerHTML =
-          "Tweet is too long. Keep it under 140 characters!";
-          return;
-        });
-    }
-
-    $("p.error").slideUp(1000, function () {
-    });
-
+    
+    //post tweet request
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: formData,
       success: (tweetResponse) => {
-        // const  clearForm = $("#clear-text-area").val();
-        $("textarea").val("");
-        $(".counter").text(140);
-        console.log("tweetResponse", tweetResponse);
-        // loadTweets();
+
+        $("textarea").val(""); //clear textarea after submit
+        $(".counter").text(140); //reset counter after submit
+
         $.get("/tweets", (tweetResponse) => {
-          // console.log("tweetRes: ", tweetResponse);
-          // console.log("latestTweet: ", tweetResponse.slice(-1));
-          renderTweets(tweetResponse.slice(-1));
+         
+          renderTweets(tweetResponse.slice(-1)); //Add newest tweet to page
         });
-      },
-      error: (err) => {
-        console.log(err, "Error loading tweets!");
-      },
+      }
     });
   });
 
+  //Takes in one tweet data object and converts it to an html article to add to the page
   const createTweetElement = function (data) {
-    console.log("dataCreated: ", timeago.format(data.created_at));
-    const $tweet = $("<article>").addClass("tweet");
+    const userData = data.user;
+    const $tweet = $("<article>").addClass("tweet"); //creating the element with a class to accept dynamic tweets
+
     const html = `
           <header>
             <div class="user-icon-and-name">
               <i class="fa-solid fa-user"></i>
-              <h5>${escape(data.user.name)}</h5>
+              <h5>${escape(userData.name)}</h5>
             </div>
-            <p>${escape(data.user.handle)}</p>
+            <p>${escape(userData.handle)}</p>
           </header>
             <p class="tweet-content">${escape(data.content.text)}</p>
           <footer>
@@ -109,14 +103,15 @@ $(document).ready(function () {
             </div>
           </footer>
         `;
-    const tweetElement = $tweet.append(html);
+    const tweetElement = $tweet.append(html); //adding the new tweet into page html
     return tweetElement;
   };
 
+
+  //takes in database array of tweet objects. Uses createTweetElement to convert to html and adds the newest tweet at the top
   const renderTweets = function (tweets) {
-    console.log("renderTweets: ", tweets);
     const $tweetContainer = $("#tweets-container");
-    // console.log($tweetContainer);
+
     for (const tweet of tweets) {
       const tweetElement = createTweetElement(tweet);
       $tweetContainer.prepend(tweetElement);
